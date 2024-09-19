0c574b93977ca352b2ad52b42bad624da9c0cd55
prasad-kate <prasadkatepk@gmail.com>
2024-09-19 12:28:45 +0530

    style: change menu item background color when active

--- src/components/RootLayout/components/MenuItem.tsx
+++ src/components/RootLayout/components/MenuItem.tsx
@@ -3,12 +3,16 @@ import { MenuItemProps } from "../../../types";
 
 function MenuItem({ name, path }: MenuItemProps) {
   const navigate = useNavigate();
+  const currentPath = location.pathname;
+  const menuItemClassName: string = `border p-2 mb-1.5 cursor-pointer rounded-md bg-gray-100 hover:bg-gray-200 text-sm ${
+    currentPath === path ? "bg-gray-200" : ""
+  }`;
   return (
     <div
       onClick={() => {
         navigate(path);
       }}
-      className="border p-2 mb-1.5 cursor-pointer rounded-md bg-gray-100 hover:bg-gray-200 text-sm"
+      className={menuItemClassName}
     >
       {name}
     </div>
